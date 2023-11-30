fd4584a75d9bd2d7b9f7b908707a7812f6209876
Giordano Bruno <132802885+777Ironhide666@users.noreply.github.com>
2023-11-30 16:46:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>curso php</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+
+<body>
+    <h1 style="text-align:center">Programando em PHP</h1>
+    <div>
+    </div>
+</body>
+
+</html>
